e0af89dc3ddae431adb319983dfc10dee5e1cc95
c1fr1 <c1fr1.stkcps@gmail.com>
2018-07-23 20:45:07 +0000

    window size is bigger

--- src/engine/EnigView.java
+++ src/engine/EnigView.java
@@ -16,7 +16,7 @@ public abstract class EnigView {
 	 * creates a new main view
 	 */
 	public EnigView() {
-		window = new EnigWindow(600, 480, getName());
+		window = new EnigWindow(1200, 1000, getName());
 		setup();
 		while ( !glfwWindowShouldClose(EnigWindow.mainWindow.id) ) {
 			window.update();
