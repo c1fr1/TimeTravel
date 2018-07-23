1df3c683c00d402846ee9ee12590babdb5f63040
Nathan <nt.rip.diver@gmail.com>
2018-07-23 11:28:03 -0700

    level class

---
+++ src/Game/LevelBase.java
@@ -0,0 +1,15 @@
+package Game;
+
+public class LevelBase
+{
+    int width;
+    int height;
+    char[][] layout = new char[width][height];
+
+    public void roomCreator(String stuff,int width,int height)
+    {
+        char[][] layout = new char[width][height];
+
+    }
+
+}
