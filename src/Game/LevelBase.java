d85fe008210534a2689b98650f12b9fda4c3a5b2
Nathan <nt.rip.diver@gmail.com>
2018-07-23 11:43:38 -0700

    level class

--- src/Game/LevelBase.java
+++ src/Game/LevelBase.java
@@ -6,10 +6,16 @@ public class LevelBase
     int height;
     char[][] layout = new char[width][height];
 
-    public void roomCreator(String stuff,int width,int height)
+    public void roomCreator(String level,int width,int height)
     {
         char[][] layout = new char[width][height];
-
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                layout[i][j] =  level.charAt(i+j);
+            }
+        }
     }
 
 }
