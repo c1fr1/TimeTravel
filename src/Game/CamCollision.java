c063180e4ae9fe98fe6b1e1f6c53e31005074d26
Gavith <gavinlesher5501@gmail.com>
2018-07-24 11:21:50 -0700

    Added "collision" checking

--- src/Game/CamCollision.java
+++ src/Game/CamCollision.java
@@ -3,19 +3,33 @@ package Game;
 import engine.Entities.Camera;
 import java.util.ArrayList;
 public class CamCollision {
-    Camera cam;
-    LevelBase level;
-
-    public CamCollision(Camera cam, LevelBase level){
-        this.cam = cam;
-        this.level = level;
-    }
-
-    public boolean collisionNS(int speed){
+    /**
+     * Using level, camera position and speed predicts what object(in character form) will be in the move
+     * @param cam Camera object
+     * @param level LevelBase object
+     * @param speed Speed of movement
+     * @return character at intended move
+     */
+    public char collisionH(Camera cam, LevelBase level, float speed){
         float x = cam.x;
         float y = cam.y;
+        float yPosition = y/50f;
         ArrayList<Character[]> current = level.levelseries.get(level.currentTZ);
+        return (current.get((int)yPosition)[(int)((x + speed)/50f)]);
 
-        return false;
+    }
+    /**
+     * Using level, camera position and speed predicts what object(in character form) will be in the move
+     * @param cam Camera object
+     * @param level LevelBase object
+     * @param speed Speed of movement
+     * @return character at intended move
+     */
+    public char collisionV(Camera cam, LevelBase level, float speed){
+        float x = cam.x;
+        float y = cam.y;
+        float xPosition = x/50f;
+        ArrayList<Character[]> current = level.levelseries.get(level.currentTZ);
+        return (current.get((int)xPosition)[(int)((y + speed)/50f)]);
     }
 }
