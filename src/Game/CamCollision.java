ac15059f5bf4bf32e60aca0fe467cefad69b8de3
Paul Simko <pasimko13@gmail.com>
2018-07-25 10:07:43 -0700

    got rid of a stupid thing i tried to fix regarding collisions bc it wasn't a complete fix

--- src/Game/CamCollision.java
+++ src/Game/CamCollision.java
@@ -11,7 +11,7 @@ public class CamCollision {
 
     public static char checkCollision(float x, float y, float hSpeed, float vSpeed, ArrayList<Character[]> room){
         Vector2f speedVector = new Vector2f(hSpeed, vSpeed);
-        Vector2f positionVector = new Vector2f(x+15,y+15);
+        Vector2f positionVector = new Vector2f(x,y);
         Vector2f totalVector = speedVector.add(positionVector);
         totalVector.set(totalVector.x/50f, totalVector.y/50f);
         return room.get((int)totalVector.y)[(int)totalVector.x];
