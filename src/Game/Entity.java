951b3c696678ab43057b7606a1d0796a7b507620
Paul Simko <pasimko13@gmail.com>
2018-07-31 10:12:27 -0700

    Your commit is writing checks your merge can't cash.

--- src/Game/Entity.java
+++ src/Game/Entity.java
@@ -59,8 +59,8 @@ public class Entity
                 if (a.ypos[timeZone] + a.border >= b.ypos[timeZone] - b.border &&
                         a.ypos[timeZone] - a.border <= b.ypos[timeZone] + b.border)
                 {
-                overlap = true;
-            }
+                    overlap = true;
+                }
             }
         }
         return overlap;
