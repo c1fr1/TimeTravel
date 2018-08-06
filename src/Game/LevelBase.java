c5a768ebf899a42db51680fcad474cdd63a261b9
Nathan <nt.rip.diver@gmail.com>
2018-08-06 11:21:33 -0700

    no-ivy floor version

--- src/Game/LevelBase.java
+++ src/Game/LevelBase.java
@@ -74,7 +74,7 @@ public class LevelBase
 	public void init(String filename, String[] textures) {
 		if (tileObj == null) {
 			tileObj = new VAO(-25f, -25f, 50f, 50f);
-			floorTexture = new Texture("res/tilesets/floor-apocalypse.png");
+			floorTexture = new Texture("res/tilesets/floor-apocalypse-no-ivy.png");
 			wallTexture = new Texture("res/tilesets/wall-apocalypse.png");
 			keyTexture = new Texture("res/sprites/inventoryKey.png");
 			endLevelTexture = new Texture("res/sprites/endLevel.png");
